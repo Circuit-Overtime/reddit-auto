3efbad2e76ddca10c36d5b098c0139f810b3fdfb
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-12-23 17:24:54 +0000

    merge from the main conflict resolve

--- src/main.ts
+++ src/main.ts
@@ -2,36 +2,6 @@ import {LINK, TITLE} from './link.js';
 import { Devvit } from '@devvit/public-api';
 
 Devvit.configure({
-<<<<<<< HEAD
-  http: {
-    domains: ['gen.pollinations.ai', 'api.github.com'],
-  },
-});
-
-
-Devvit.addSettings([
-  {
-    type: 'string',
-    name: 'gh_token',
-    label: 'GitHub Token',
-    isSecret: true,
-    scope: SettingScope.App, 
-  },
-  {
-    type: 'string',
-    name: 'p_key',
-    label: 'Polli Key',
-    isSecret: true,
-    scope: SettingScope.App,
-  },
-])
-
-Devvit.addMenuItem({
-  label: 'Post Pollinations Image',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async (_, context) => {
-=======
   redditAPI: true,
   media: true,
 });
@@ -39,7 +9,6 @@ Devvit.addMenuItem({
 Devvit.addTrigger({
   event: 'AppUpgrade',
   onEvent: async (event, context) => {
->>>>>>> aae439bf521c71f7e853e48cc3083318f201c05d
     try {
       const imageAsset = await context.media.upload({
         url: LINK,
